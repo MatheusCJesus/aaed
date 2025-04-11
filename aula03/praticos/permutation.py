b4ef930bccb17d1d31fd03fab64fa878a96b1d7b
MatheusCJesus <matheus.cerqueira100@gmail.com>
2025-04-10 22:56:04 -0300

    refactor permutation function to use lexicographic ordering for generating permutations

--- aula03/praticos/permutation.py
+++ aula03/praticos/permutation.py
@@ -74,23 +74,43 @@ bubble_sort(str_to_list("bacrtg"))
 def permutation(string):
     """
     This function takes a string and returns all the permutations of the string in ascending order.
+    Uses a non-recursive approach based on lexicographic ordering.
     """
-    
     # Convert the string to a list of characters
-    list = str_to_list(string)
+    chars = str_to_list(string)
     
-    # Sort the list of characters
-    sorted_list = bubble_sort(list)
+    # Sort the list of characters to start with the first permutation
+    chars = bubble_sort(chars)
     
-    sorted_permutations = []
+    # Initialize the result list with the first permutation
+    result = [''.join(chars)]
+    
+    while True:
+        # 1. Find the largest index i such that chars[i] < chars[i+1]
+        i = len(chars) - 2
+        while i >= 0 and chars[i] >= chars[i+1]:
+            i -= 1
+            
+        # If no such index exists, we've generated all permutations
+        if i < 0:
+            break
+            
+        # 2. Find the largest index j > i such that chars[j] > chars[i]
+        j = len(chars) - 1
+        while chars[j] <= chars[i]:
+            j -= 1
+            
+        # 3. Swap chars[i] and chars[j]
+        chars[i], chars[j] = chars[j], chars[i]
+        
+        # 4. Reverse the suffix starting at chars[i+1]
+        chars[i+1:] = chars[i+1:][::-1]
+        
+        # Add the new permutation to the result
+        result.append(''.join(chars))
+    
+    return result
 
-    # Generate all permutations of the string
-    for i in range(len(sorted_list)):
-        for j in range(i+1, len(sorted_list)):
-            # Swap the characters
-            sorted_list[i], sorted_list[j] = sorted_list[j], sorted_list[i]
-            # Append the permutation to the list
-            sorted_permutations.append("".join(sorted_list))
-            # Swap back the characters
-            sorted_list[i], sorted_list[j] = sorted_list[j], sorted_list[i]
-
+# %%
+permutation("abc")
+# %%
